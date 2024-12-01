4b1c41fcddf17a07be26dc694bf6eaadbb4f5596
Elmer Dema <elmerdema2022@gmail.com>
2024-12-01 11:50:50 +0100

    setting up the database +serialization

---
+++ src/models.rs
@@ -0,0 +1,22 @@
+#[derive(Queryable, Serialize)]
+pub struct Book {
+    pub id: i32,
+    pub name: String,
+    pub author: String,
+}
+
+#[derive(Debug, Insertable, AsChangeset, Serialize, Deserialize, Clone)]
+#[table_name = "books"]
+pub struct NewBook {
+    pub name: String,
+    pub author: String,
+}
+
+impl From<web::Json<NewBook>> for NewBook {
+    fn from(book: web::Json<NewBook>) -> Self {
+        NewBook {
+            name: book.name.clone(),
+            author: book.author.clone(),
+        }
+    }
+}
---
+++ src/schema.rs
@@ -0,0 +1,7 @@
+diesel::table! {
+    books (id) {
+        id -> Int4,
+        name -> Varchar,
+        author -> Varchar,
+    }
+}
